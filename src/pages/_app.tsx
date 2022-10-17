9e2b5ffab920252c9d73d3aa72e25f851e1913c7
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-10-17 15:51:24 +0200

    SPINNING

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>three - wiktrek.xyz</title>
+        <meta
+          name="keywords"
+          content="wiktrek, wiktrek.xyz, wiktrek website, wiktor, wiktorek, xyz, wiktrek xyz, wiktor, wiktorek"
+        />
+        <meta name="description" content="wiktrek's website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
