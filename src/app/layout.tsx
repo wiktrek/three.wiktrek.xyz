82cbf21b2fbe086258d3276b5c8b3419aa813eb1
wiktrek <wiktor.janiszewski10@gmail.com>
2023-12-20 23:20:41 +0100

    feat: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'three.wiktrek.xyz',
   description: 'App made with three.js',
+  icons: ['/favicon.svg'],
 };
 
 export default function RootLayout({
@@ -16,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="bg-background text-text">
+      <head></head>
       <body className={inter.className}>{children}</body>
     </html>
   );
