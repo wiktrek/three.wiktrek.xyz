2b30644bc1df629eca91a492b578514577308f09
wiktrek <wiktor.janiszewski10@gmail.com>
2023-12-23 15:57:55 +0100

    feat: removed model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,15 @@ import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Box from '~/components/Box';
-import Model from '~/components/Model';
 export default function Home() {
   return (
     <main className="text-xl justify-center items-center h-screen w-screen">
       <Canvas>
-        <hemisphereLight args={['#fff', '#333', Math.PI]} />
+        <hemisphereLight args={['#fff', '#333', 5]} />
         <color attach="background" args={['#05080a']} />
         <Box position={[-2, 0, 0]} color="red" />
         <Box position={[0, 0, 0]} color="green" />
         <Box position={[2, 0, 0]} />
-        <Model />
         <OrbitControls />
       </Canvas>
     </main>
