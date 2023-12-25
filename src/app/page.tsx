f957d66ad2cd080cd2cb8887ffaccb8f0ce07111
wiktrek <wiktor.janiszewski10@gmail.com>
2023-12-25 23:43:18 +0100

    feat: cup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,17 @@ import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Box from '~/components/Box';
+import Cup from '~/components/Cup';
 export default function Home() {
   return (
     <main className="text-xl justify-center items-center h-screen w-screen">
-      <Canvas>
+      <Canvas camera={{ fov: 60 }}>
         <hemisphereLight args={['#fff', '#333', 5]} />
-        <color attach="background" args={['#05080a']} />
+        <color attach="background" args={['#fffff']} />
         <Box position={[-2, 0, 0]} color="red" />
         <Box position={[0, 0, 0]} color="green" />
         <Box position={[2, 0, 0]} />
+        <Cup />
         <OrbitControls />
       </Canvas>
     </main>
