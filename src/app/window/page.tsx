a0b4e738cb24da52ce8fdabc6e11459a2f23cea8
wiktrek <wiktor.janiszewski10@gmail.com>
2024-01-11 15:31:00 +0100

    .

--- src/app/window/page.tsx
+++ src/app/window/page.tsx
@@ -3,65 +3,25 @@ interface Window {
   id: number;
 }
 import * as THREE from 'three';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { HexColorPicker } from 'react-colorful';
-import Box from '~/components/Box';
-import Model from '~/components/Cup';
-import { useEffect, useState } from 'react';
-import { useAtom } from 'jotai';
+import { useEffect, useRef, useState } from 'react';
+import { Provider, atom, createStore, getDefaultStore, useAtom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
-const windowAtom = atomWithStorage('windows', [{}] as Window[]);
+
+const store = getDefaultStore();
+// const windowAtom = atomWithStorage('windows', [] as Window[]);
+const windowAtom = atom([] as Window[]);
+const sub = store.sub(windowAtom, () => {
+  console.log(store);
+});
+
 export default function Home() {
   const [windows, setWindows] = useAtom(windowAtom);
   const [color, setColor] = useState('#99ff99');
-  const a = {
-    id: Number(Math.random() * 10),
-  } as Window;
-  function Picker() {
-    return (
-      <div className="absolute bottom-0 left-0">
-        <HexColorPicker color={color} onChange={(c) => setColor(c)} />
+  return (
+    <Provider store={store}>
+      <div className="text-xl justify-center items-center h-screen w-screen overflow-hidden relative">
+        <p>wiktrek</p>
       </div>
-    );
-  }
-  function addWindow(p: Window) {
-    let ww = windows;
-    windows.map((w) => {
-      if (p.id == w.id) {
-        return;
-      }
-    });
-    ww.push(p);
-    setWindows(ww);
-  }
-  useEffect(() => {
-    addWindow(a);
-  });
-  window.addEventListener('beforeunload', function (e) {
-    let index = windows.indexOf(a);
-    setWindows(windows.splice(index, 1));
-  });
-  return (
-    <main className="text-xl justify-center items-center h-screen w-screen overflow-hidden relative">
-      <Canvas camera={{ fov: 60 }}>
-        <hemisphereLight args={['#fff', '#333', 5]} />
-        <color attach="background" args={['#05080a']} />
-        <Box position={[-2, 0, 0]} color="red" />
-        <Box position={[0, 0, 0]} color="green" />
-        <Box position={[2, 0, 0]} />
-        <Cup color={color} />
-        <OrbitControls />
-      </Canvas>
-
-      <Picker />
-    </main>
-  );
-}
-function Cup(props: { color: string }) {
-  return (
-    <>
-      <Model color={props.color} />
-    </>
+    </Provider>
   );
 }
